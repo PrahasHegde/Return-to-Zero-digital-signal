5910d7b10b5efa6cf62632d5246f5543d6bd1f95
prahas10 <108119173+PrahasHegde@users.noreply.github.com>
2023-10-10 19:37:28 +0530

    Update RZip.py
    RZ digital signal with user input

--- RZip.py
+++ RZip.py
@@ -1,36 +1,32 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-# Get user input for the binary sequence (0s and 1s)
-binary_sequence = input("Enter a binary sequence (e.g., 010101): ")
+def encode(data):
+    encoded_data = []
+    if data[0] == '0':
+        encoded_data.append(-1)
+    else:
+        encoded_data.append(1)
+    for i in data:
+        if i == '0':
+            encoded_data.append(-1)
+            encoded_data.append(0)
+        elif i == '1':
+            encoded_data.append(1)
+            encoded_data.append(0)
+        else:
+            print('invalid input')
+    return encoded_data
 
-# Define signal parameters
-bit_duration = 1.0  # Duration of each bit in seconds
-bit_rate = 1 / bit_duration  # Bit rate in bits per second
 
-# Create a time array for the signal
-t = np.linspace(0, len(binary_sequence) * bit_duration, len(binary_sequence) * 1000)
+data = input('enter the data: ')
 
-# Initialize an empty signal
-signal = np.zeros_like(t)
+y = encode(data=data)
 
-# Create the RZ signal based on the binary input
-for i, bit in enumerate(binary_sequence):
-    bit_start = i * bit_duration
-    bit_middle = bit_start + bit_duration / 2
-
-    if bit == '1':
-        # Set the signal to a positive pulse for '1'
-        signal[(t >= bit_start) & (t <= bit_middle)] = 1
-    elif bit == '0':
-        # Set the signal to zero for '0'
-        signal[(t >= bit_start) & (t <= bit_middle)] = 0
-
-# Plot the RZ signal
-plt.plot(t, signal)
-plt.title("Return to Zero (RZ) Digital Signal")
-plt.xlabel("Time (s)")
-plt.ylabel("Amplitude")
-# plt.ylim(-0.1, 1.1)
-plt.grid(True)
+x = [i for i in range(len(y))]
+plt.step(x ,y)
+plt.xlabel('time')
+plt.ylabel('voltage')
+plt.title('RZ encoding')
+print(y)
+print(x)
 plt.show()
